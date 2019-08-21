8dd2a95260584e67bbfaea4f18ad49c3b2e10db2
Kushal Chakrabarti <kushalc@talent.works>
2019-08-20 18:38:45 -0700

    Implemented E2E transcriber.

--- util/aws/transcribe.py
+++ util/aws/transcribe.py
@@ -1,4 +1,10 @@
 import json
+import logging
+import os
+import time
+import urllib
+
+import boto3
 import numpy as np
 import pandas as pd
 
@@ -38,4 +44,43 @@ def _parse_transcription(handle, speaker_labels={}):
 
         transcript_df = speaker_df[["speaker", "content", "confidence", "start_time", "end_time"]]
 
+    return transcript_df
+
+def _transcribe_audio(s3_target, s3_source, name=None, speaker_ct=2,
+                      language="en-US", region="us-west-1", retries=10):
+    client = boto3.client("transcribe")
+
+    job_name = name or s3_source
+    s3_components = urllib.parse.urlparse(s3_source)
+    client.start_transcription_job(**{
+        "TranscriptionJobName": job_name,
+        "LanguageCode": language,
+        "MediaFormat": os.path.splitext(s3_components.path)[-1],
+        "Media": {
+            "MediaFileUri": f"https://s3-{ region }.amazon.aws.com/{ s3_components.netloc }/{ s3_components.path }",
+        },
+        "OutputBucketName": urllib.parse.urlparse(s3_source).netloc,
+        "Settings": {
+            "ShowSpeakerLabels": True,
+            "MaxSpeakerLabels": speaker_ct,
+        }
+    })
+
+    for ix in range(retries):
+        job = client.get_transcription_job(TranscriptionJobName=job_name)
+        if job["TranscriptionJobStatus"] != "IN_PROGRESS":
+            break
+
+        sleep_s = 2.000 ** ix
+        logging.warn("Retrying %s after %.3f seconds", job_name, sleep_s)
+        time.sleep(sleep_s)
+
+    # FIXME: Move this to s3_target instead of overwriting it.
+    s3_interim_path = job.get("Transcript", {}).get("TranscriptFileUri")
+    if job["TranscriptionJobStatus"] != "COMPLETED":
+        logging.error("Couldn't complete %s job: %s [%s]: %s", job_name, job["TranscriptionJobStatus"],
+                      job.get("FailureReason"), s3_interim_path)
+        return None
+
+    transcript_df = _parse_transcription(s3_interim_path)
     return transcript_df
