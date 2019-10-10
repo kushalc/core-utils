9526f8b6736405f4845a7a90e47555d84dbe3091
Kushal Chakrabarti <kushalc@talent.works>
2019-10-10 12:44:44 -0400

    Handled dict edge case.

--- util/parallelization.py
+++ util/parallelization.py
@@ -10,7 +10,7 @@ def parallel_apply(data, func, process_ct=2, direct_apply=False, **kwargs):
 
 def _parallelize(df, func, process_ct=2):
     pool = mp.Pool(process_ct)
-    split_dfs = np.array_split(df, process_ct)
+    split_dfs = np.array_split(df, process_ct)  # returns list of np.ndarrays if input is list
     data = pd.concat(pool.map(func, split_dfs))
     pool.close()
     pool.join()
@@ -19,16 +19,23 @@ def _parallelize(df, func, process_ct=2):
 def _run_on_subset(func, subset_df_or_s, direct_apply=False, **kwargs):
     if direct_apply:
         return func(subset_df_or_s, **kwargs)
+
     if isinstance(subset_df_or_s, pd.DataFrame):
         return subset_df_or_s.apply(func, axis=1, **kwargs)
+
     elif isinstance(subset_df_or_s, (np.ndarray, pd.Series)):
         results = [func(item, **kwargs) for item in subset_df_or_s]
+
+        index = range(len(results))
+        if hasattr(subset_df_or_s, "index"):
+            index = subset_df_or_s.index
+
         if isinstance(results[0], (pd.Series, pd.DataFrame)):
             return pd.concat(results)
+        elif isinstance(results[0], dict):
+            return pd.DataFrame(results, index=index)
         else:
-            index = range(len(results))
-            if hasattr(subset_df_or_s, "index"):
-                index = subset_df_or_s.index
             return pd.Series(results, index=index)
+
     else:
         raise ValueError("Unknown class: %s" % subset_df_or_s.__class__)
