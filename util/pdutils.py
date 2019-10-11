d95f9f677e531830c99e828046f9db245a5e1f2d
Kushal Chakrabarti <kushalc@obviouslywrong.org>
2019-10-10 23:08:28 -0400

    Added pdutils w/safe_len, not_empty and safety_wrap (#9)
    * Added safe_len and notempty.
    
    * Added safe_get and notempty.
    
    * Added safety_wrap.

---
+++ util/pdutils.py
@@ -0,0 +1,36 @@
+import logging
+
+import numpy as np
+import pandas as pd
+
+def not_empty(df):
+    mask = pd.notnull(df) & \
+           df.applymap(safe_len) > 0
+    return mask
+
+def safety_wrap(method, *nargs, loglevel=logging.NOTSET, **kwargs):
+    def _safety_wrapper(value):
+        try:
+            return method(value, *nargs, **kwargs)
+        except:
+            logging.log(loglevel, "Couldn't apply %s: %s", method, value)
+            return kwargs.get("default", np.nan)
+
+    return _safety_wrapper
+
+def safe_get(dt, key, default=np.nan):
+    if dt in [np.nan, None]:
+        return default
+    value = dt.get(key, default)
+    return value
+
+def safe_len(it):
+    if it in [np.nan, None]:
+        return np.nan
+    elif hasattr(it, "__iter__") or hasattr(it, "__getitem__"):
+        try:
+            return len(it)
+        except:
+            return np.nan
+    else:
+        return 1
